37ca88e0bcc97de4e833c6fbf022ee2825cd050f
JedrzejMikolajczyk <jedrek.mikolajczyk.zs@gmail.com>
2022-09-15 23:10:13 +0200

    Update predictor.py

--- predictor.py
+++ predictor.py
@@ -22,11 +22,20 @@ class Predictor():
         self.load_model("CNN")
       
     #load json file
-    def load_from_json(self, filename):
-        with open(filename, "r") as settings:
+    def load_from_json(self, json_file):
+        with open(json_file, "r") as settings:
             models_dict = json.load(settings)
         return models_dict
       
+    def write_to_json(self, json_file, model_name, model_file, weights_file):
+        with open(json_file, "r") as settings:
+            models_dict = json.load(settings)
+        models_dict[model_name] = {
+            "model": model_file,
+            "weights": weights_file}
+        with open(json_file, "w") as settings:
+            settings.write(json.dumps(models_dict))
+    
         
     #{name:[model_filename, weights_filename]}
     def import_models(self, models_dict):
